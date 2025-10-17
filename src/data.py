6218db78598b2628c0c9d88238520502d8e23ab2
Marcus Bluestone <marcusbl@mit.edu>
2025-10-17 11:05:10 -0400

    quantile bounds for min-max

--- src/data.py
+++ src/data.py
@@ -13,14 +13,22 @@ import torchvision.transforms.functional as F
 import torch
 
 class MinMaxNormalize:
-    def __init__(self, min_val=0.0, max_val=1.0):
+    """
+    Normalizes an image so that all values run approx between min_val and max_val. 
+    
+    There's an option for choosing min = perc, max = 1 - perc to avoid
+    outliers messing up the distribution. 
+    """
+    def __init__(self, min_val=0.0, max_val=1.0, perc = 0):
         self.min_val = min_val
         self.max_val = max_val
+        self.perc = perc
 
     def __call__(self, img):
         # img assumed to be a torch.Tensor (C, H, W)
-        img_min = img.min()
-        img_max = img.max()
+        img_min = torch.quantile(img, self.perc)
+        img_max = torch.quantile(img, 1 - self.perc)
+
         img = (img - img_min) / (img_max - img_min)  # scale to 0-1
         img = img * (self.max_val - self.min_val) + self.min_val
         return img
@@ -36,7 +44,7 @@ class DicomDataset(Dataset):
     default_transform = transforms.Compose([
             transforms.ToTensor(),
             transforms.Resize((244, 244)),
-            MinMaxNormalize(0.0, 1.0),
+            MinMaxNormalize(0.0, 1.0, perc = 0.02),
             transforms.Lambda(lambda x: x.repeat(3, 1, 1))
     ])
 
@@ -207,7 +215,7 @@ def apply_augs(train_dataset: DicomDataset, val_dataset: DicomDataset, method = 
     basics = [
         transforms.ToTensor(), # doesn't scale 
         transforms.Resize((244, 244)),
-        MinMaxNormalize(0.0, 1.0),
+        MinMaxNormalize(0.0, 1.0, perc = 0.02),
         transforms.Lambda(lambda x: x.repeat(3, 1, 1))
     ]
 
